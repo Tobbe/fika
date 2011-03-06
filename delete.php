c5ce3b82dad8758dbce8cb4ca44090c6642e3840
Tobbe Lundberg <tobbesweden@gmail.com>
2011-03-06 16:34:15 -0500

    Fixed infinite loop when deleting first person in list

--- delete.php
+++ delete.php
@@ -2,12 +2,30 @@
 
 if (isset($_GET) && isset($_GET['acronym']) && $_GET['acronym'] != '') {
 	$personList = unserialize(file_get_contents('fikalist.dat'));
+	$state = unserialize(file_get_contents('state.dat'));
 
+	$updateStartPerson = false;
 	$newPersonList = array();
 	foreach ($personList as $person) {
+		if ($updateStartPerson) {
+			$state['startPerson'] = $person['acronym'];
+			$updateStartPerson = false;
+			file_put_contents('state.dat', serialize($state));
+		}
+
 		if ($person['acronym'] != $_GET['acronym']) {
 			$newPersonList[] = $person;
+		} else {
+			if ($person['acronym'] == $state['startPerson']) {
+				$updateStartPerson = true;
+			}
 		}
+	}
+
+	if ($updateStartPerson) {
+		$state['startPerson'] = $personList[0]['acronym'];
+		$updateStartPerson = false;
+		file_put_contents('state.dat', serialize($state));
 	}
 
 	file_put_contents('fikalist.dat', serialize($newPersonList));
