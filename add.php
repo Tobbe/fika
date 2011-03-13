c956051533ffc42bd4bb9a94d68d7f60edad5a3f
Tobbe Lundberg <tobbesweden@gmail.com>
2011-03-13 12:14:14 -0400

    Added lame email scraping prevention

--- add.php
+++ add.php
@@ -1,7 +1,11 @@
 <?php
+	function kanelbullify($acronym) {
+		return str_replace('@', '[kanelbulle]', $acronym);
+	}
+
 	if (isset($_POST) && isset($_POST['personname']) && $_POST['personname'] != '') {
 		$personList = unserialize(file_get_contents('fikalist.dat'));
-		$personList[] = array('name' => $_POST['personname'], 'acronym' => $_POST['acronym']);
+		$personList[] = array('name' => trim($_POST['personname']), 'acronym' => kanelbullify(trim($_POST['acronym'])));
 		file_put_contents('fikalist.dat', serialize($personList));
 	}
 
