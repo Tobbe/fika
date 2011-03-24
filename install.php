038cb9b2c116febbaf6e3b430f6fc3afaf7609ad
Tobbe Lundberg <tobbesweden@gmail.com>
2011-03-24 09:44:03 -0400

    Made install.php use a html template

--- install.php
+++ install.php
@@ -1,32 +1,39 @@
 <?php
-
 include 'FikaDate.php';
-$personList = array(
-	array('name' => 'Adam Aaronsson', 'acronym' => 'aaa'), 
-	array('name' => 'Boris Bertilsson', 'acronym' => 'bbe'),
-	array('name' => 'Ceasar Carlsson', 'acronym' => 'cca'),
-	array('name' => 'Dennis Davidsson', 'acronym' => 'dda'),
-	array('name' => 'Einar Eriksson', 'acronym' => 'eer'),
-	array('name' => 'Felix Fredriksson', 'acronym' => 'ffr'),
-	array('name' => 'G&ouml;ran Gottfriedsson', 'acronym' => 'ggo'),
-	array('name' => 'Hans Henriksson', 'acronym' => 'hhe'),
-	array('name' => 'Ibsen Ingolfsson', 'acronym' => 'iin'),
-	array('name' => 'Jakob Jansson', 'acronym' => 'jja'),
-	array('name' => 'Krister K&aring;resson', 'acronym' => 'kka'),
-	array('name' => 'Ludwig Larsson', 'acronym' => 'lla')
-);
-file_put_contents('fikalist.dat', serialize($personList));
 
-$state = array('startWeek' => 49, 'startYear' => 2010, 'startPerson' => 'aaa');
-file_put_contents('state.dat', serialize($state));
+if ($_POST) {
+	var_dump($_POST); die();
+}
 
-$tempMods = array(
-	/*array('acronym' => 'cca', 'where' => 'up', 'when' => new FikaDate(2011, 11)),
-	array('acronym' => 'cca', 'where' => 'up', 'when' => new FikaDate(2011, 10)),
-	array('acronym' => 'aaa', 'where' => 'down', 'when' => new FikaDate(2011, 10)),
-	array('acronym' => 'aaa', 'where' => 'down', 'when' => new FikaDate(2011, 11)),
-	array('acronym' => 'aaa', 'where' => 'down', 'when' => new FikaDate(2011, 12))*/
-);
-file_put_contents('tempmods.dat', serialize($tempMods));
+if ($_POST && $_POST['install']) {
+	$personList = array(
+		array('name' => 'Adam Aaronsson', 'acronym' => 'aaa'), 
+		array('name' => 'Boris Bertilsson', 'acronym' => 'bbe'),
+		array('name' => 'Ceasar Carlsson', 'acronym' => 'cca'),
+		array('name' => 'Dennis Davidsson', 'acronym' => 'dda'),
+		array('name' => 'Einar Eriksson', 'acronym' => 'eer'),
+		array('name' => 'Felix Fredriksson', 'acronym' => 'ffr'),
+		array('name' => 'G&ouml;ran Gottfriedsson', 'acronym' => 'ggo'),
+		array('name' => 'Hans Henriksson', 'acronym' => 'hhe'),
+		array('name' => 'Ibsen Ingolfsson', 'acronym' => 'iin'),
+		array('name' => 'Jakob Jansson', 'acronym' => 'jja'),
+		array('name' => 'Krister K&aring;resson', 'acronym' => 'kka'),
+		array('name' => 'Ludwig Larsson', 'acronym' => 'lla')
+	);
+	file_put_contents('fikalist.dat', serialize($personList));
 
+	$state = array('startWeek' => 49, 'startYear' => 2010, 'startPerson' => 'aaa');
+	file_put_contents('state.dat', serialize($state));
+
+	$tempMods = array(
+		/*array('acronym' => 'cca', 'where' => 'up', 'when' => new FikaDate(2011, 11)),
+		array('acronym' => 'cca', 'where' => 'up', 'when' => new FikaDate(2011, 10)),
+		array('acronym' => 'aaa', 'where' => 'down', 'when' => new FikaDate(2011, 10)),
+		array('acronym' => 'aaa', 'where' => 'down', 'when' => new FikaDate(2011, 11)),
+		array('acronym' => 'aaa', 'where' => 'down', 'when' => new FikaDate(2011, 12))*/
+	);
+	file_put_contents('tempmods.dat', serialize($tempMods));
+}
+
+include 'install.tpl.php';
 ?>
---
+++ install.tpl.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+	<title>Fika list installation</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="Language" content="English, en-EN">
+	<meta name="Author" content="Tobbe Lundberg">
+	<meta name="Robots" content="index,follow">
+	<meta http-equiv="Pragma" content="no-cache">
+	<meta http-equiv="Expires" content="-1">
+
+	<link rel="stylesheet" href="style.css" type="text/css" media="screen">
+</head>
+<body>
+<form method="post" action="install.php">
+	<input type="submit" name="install" value="Reset default values">
+</form>
+</body>
+</html>
+
